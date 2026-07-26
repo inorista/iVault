f3dac5ab2de88d4f1a6c0b15b66480a1117a7061
Ngo Quoc Tu <ngotu2102@gmail.com>
2026-07-26 16:33:34 +0700

    Initial Commit

---
+++ iVault/iVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  iVaultApp.swift
+//  iVault
+//
+//  Created by Tu on 26/7/26.
+//
+
+import SwiftUI
+
+@main
+struct iVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iVaultTests/iVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  iVaultTests.swift
+//  iVaultTests
+//
+//  Created by Tu on 26/7/26.
+//
+
+import Testing
+
+struct iVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
